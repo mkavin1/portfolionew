7c3b58fc6e7c2f4889608b1e50bfade5f6e293ac
mkavin1 <144508762+mkavin1@users.noreply.github.com>
2024-06-02 16:07:28 +0530

    Update Sidebar.js

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -7,18 +7,6 @@ import { AiFillLinkedin, AiFillGithub, AiOutlineUnorderedList } from 'react-icon
 import { NavLink } from "react-router-dom";
 
 function Sidebar() {
-  // Define unused variables to avoid ESLint errors
-  const unusedVscHome = VscHome;
-  const unusedFiUser = FiUser;
-  const unusedBsCode = BsCode;
-  const unusedMdOutlineDashboard = MdOutlineDashboard;
-  const unusedMdOutlineContactPage = MdOutlineContactPage;
-  const unusedMdEmail = MdEmail;
-  const unusedAiFillLinkedin = AiFillLinkedin;
-  const unusedAiFillGithub = AiFillGithub;
-  const unusedAiOutlineUnorderedList = AiOutlineUnorderedList;
-  const unusedNavLink = NavLink;
-
   return (
     <Fragment>
       <div className="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark slide-in-left">
